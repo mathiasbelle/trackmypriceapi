cd7efab12c61c60ab10bb4fc7b4663868f9c1949
Mathias Bellé <mathiasbelle@hotmail.com>
2025-03-18 00:05:11 -0300

    refactor: update config to load values from env variables

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,10 @@
-import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+    Inject,
+    Injectable,
+    InternalServerErrorException,
+    Logger,
+    OnModuleInit,
+} from '@nestjs/common';
 import { Pool, QueryResult } from 'pg';
 
 @Injectable()
@@ -7,11 +13,19 @@ export class DatabaseService implements OnModuleInit {
 
     constructor(@Inject('DATABASE_POOL') private readonly pool: Pool) {}
 
-    async executeQuery(query: string, params?: any[]): Promise<any[]> {
-        this.logger.debug(`Executing query: ${query} ${params}`);
-        const res: QueryResult = await this.pool.query(query, params);
-        this.logger.debug(`Query result: ${res}`);
-        return res.rows;
+    async executeQuery(query: string, params?: any[]): Promise<QueryResult> {
+        this.logger.debug(
+            `Executing query: ${query} with params ${JSON.stringify(params)}`,
+        );
+        try {
+            const res: QueryResult = await this.pool.query(query, params);
+            this.logger.debug(`Query result: ${JSON.stringify(res.rows)}`);
+            return res;
+        } catch (error) {
+            throw new InternalServerErrorException(
+                `Error when executing query: ${query}`,
+            );
+        }
     }
 
     async onModuleInit() {
@@ -23,25 +37,57 @@ export class DatabaseService implements OnModuleInit {
         try {
             this.logger.log('Checking and creating tables...');
 
-            await client.query(`
-            CREATE TABLE IF NOT EXISTS users (
-              id SERIAL PRIMARY KEY,
-              email VARCHAR(255) NOT NULL UNIQUE,
-              password VARCHAR(255) NOT NULL,
-              created_at TIMESTAMPTZ DEFAULT NOW()
-            );
-          `);
+            // try {
+            //     await client.query(
+            //         `CREATE TYPE user_role AS ENUM ('${Role.ADMIN}', '${Role.USER}');`,
+            //     );
+            // } catch (error) {
+            //     if (error.code !== '42710') {
+            //         // 42710: duplicate_object
+            //         throw error;
+            //     }
+            // }
+
+            //     await client.query(`
+            //     CREATE TABLE IF NOT EXISTS users (
+            //       id SERIAL PRIMARY KEY,
+            //       role user_role NOT NULL DEFAULT '${Role.USER}',
+            //       name VARCHAR(255) NOT NULL,
+            //       email VARCHAR(255) NOT NULL UNIQUE,
+            //       password VARCHAR(255) NOT NULL,
+            //       created_at TIMESTAMPTZ DEFAULT NOW(),
+            //       updated_at TIMESTAMPTZ DEFAULT NOW()
+            //     );
+            //   `);
 
             await client.query(`
             CREATE TABLE IF NOT EXISTS products (
               id SERIAL PRIMARY KEY,
-              name VARCHAR(255) NOT NULL,
-              price NUMERIC(10, 2) NOT NULL,
+              name TEXT NOT NULL,
+              current_price NUMERIC(12, 2) NOT NULL,
               url TEXT NOT NULL,
-              user_id INTEGER NOT NULL REFERENCES users(id),
-              created_at TIMESTAMPTZ DEFAULT NOW()
+              user_uid TEXT NOT NULL,
+              user_email TEXT NOT NULL,
+              last_checked_at TIMESTAMPTZ DEFAULT NOW(),
+              created_at TIMESTAMPTZ DEFAULT NOW(),
+              updated_at TIMESTAMPTZ DEFAULT NOW()
             );
           `);
+
+            await client.query(`
+                CREATE OR REPLACE FUNCTION update_updated_at_column()
+                RETURNS TRIGGER AS $$
+                BEGIN
+                    NEW.updated_at = NOW();
+                    RETURN NEW;
+                END;
+                $$ LANGUAGE plpgsql;
+
+                CREATE TRIGGER set_updated_at
+                BEFORE UPDATE ON products
+                FOR EACH ROW
+                EXECUTE FUNCTION update_updated_at_column();
+            `);
 
             this.logger.log('Tables checked and created.');
         } catch (error) {
