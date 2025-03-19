dcbef5743b907168c0fdade23e4effe8a6f0ceb0
Mathias Bellé <mathiasbelle@hotmail.com>
2025-03-19 16:19:57 -0300

    feat: add the new modules to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { UsersController } from './users/users.controller';
+import { ProductsModule } from './products/products.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { PricetrackerModule } from './pricetracker/pricetracker.module';
+import { ScraperModule } from './scraper/scraper.module';
+import { MailerModule } from './mailer/mailer.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true}), DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        ScheduleModule.forRoot(),
+        DatabaseModule,
+        UsersModule,
+        ProductsModule,
+        PricetrackerModule,
+        ScraperModule,
+        MailerModule,
+    ],
+    controllers: [AppController, UsersController],
+    providers: [AppService],
 })
 export class AppModule {}
