ddc1f8696883ec7ee96ba0975265bee4b80af844
Mathias Bellé <mathiasbelle@hotmail.com>
2025-03-19 16:24:28 -0300

    feat: add conditions to the logger based on running environment

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+    const app = await NestFactory.create(AppModule, {
+        cors: true,
+        logger:
+            process.env.NODE_ENV === 'development' ||
+            process.env.NODE_ENV === 'debug'
+                ? ['log', 'debug', 'error', 'verbose', 'warn']
+                : ['error', 'warn'],
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
