c5cd4939aaa8ddf830896f47404810fbaeb49caf
Mathias Bellé <mathiasbelle@hotmail.com>
2025-04-10 18:31:51 -0300

    feat: add the swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
@@ -12,6 +13,16 @@ async function bootstrap() {
                 : ['error', 'warn'],
     });
     app.useGlobalPipes(new ValidationPipe());
+
+    const config = new DocumentBuilder()
+        .setTitle('TrackMyPrice API')
+        .setDescription('The TrackMyPrice API description')
+        .setVersion('1.0')
+        .addTag('trackmyprice, api, tracking, prices, scraping, scraper')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/', app, documentFactory);
+
     await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
