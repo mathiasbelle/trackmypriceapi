515c7c869e16bd4983a3ed9c3c6384282f48626d
Mathias Bellé <mathiasbelle@hotmail.com>
2025-04-25 17:57:59 -0300

    test(mailer): define the unit tests for the mailer service

--- src/mailer/mailer.service.spec.ts
+++ src/mailer/mailer.service.spec.ts
@@ -1,18 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MailerService } from './mailer.service';
+import { Transporter } from 'nodemailer';
+import { ConfigModule } from '@nestjs/config';
 
 describe('MailerService', () => {
-  let service: MailerService;
+    let service: MailerService;
+    let transportMock: Partial<Transporter>;
+    beforeEach(async () => {
+        transportMock = {
+            sendMail: jest.fn(),
+        };
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [ConfigModule],
+            providers: [
+                { provide: 'TRANSPORTER', useValue: transportMock },
+                MailerService,
+            ],
+        }).compile();
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [MailerService],
-    }).compile();
+        service = module.get<MailerService>(MailerService);
+    });
 
-    service = module.get<MailerService>(MailerService);
-  });
-
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
