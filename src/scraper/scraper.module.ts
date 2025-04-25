3df8b616e3ea91177169daff4beb7e5f7e26d192
Mathias Bellé <mathiasbelle@hotmail.com>
2025-04-25 17:01:35 -0300

    chore: remove unnecessary imports

--- src/scraper/scraper.module.ts
+++ src/scraper/scraper.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ScraperService } from './scraper.service';
-import { DatabaseModule } from 'src/database/database.module';
-import { DatabaseService } from 'src/database/database.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
